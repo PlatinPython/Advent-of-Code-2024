0899dcc89af67be29c107bc35d5cf851c934ce48
PlatinPython <robert.alexander.kaempf@gmail.com>
2024-12-03 14:46:42 +0100

    Rework Day 3 without Regex

--- day03/day03.hs
+++ day03/day03.hs
@@ -1,27 +1,29 @@
 import Control.Arrow (ArrowChoice (right))
-import Data.Either (rights)
 import Data.Functor (($>))
-import Text.Parsec (between, parse, sepBy1, (<|>))
+import Data.Maybe (catMaybes)
+import Text.Parsec (anyChar, between, many, parse, sepBy1, (<|>))
 import Text.Parsec.Char (char, string)
 import Text.Parsec.Prim (try)
 import Text.Parsec.String (Parser)
 import Text.ParserCombinators.Parsec.Number (int)
-import Text.Regex.TDFA (getAllTextMatches, (=~))
 
 part1 :: FilePath -> IO Int
-part1 file = do
-  contents <- readFile file
-  return . sum . map (uncurry (*)) . rights . map (parse parseMul mempty) . getAllTextMatches $ contents =~ "mul\\([0-9]{1,3},[0-9]{1,3}\\)"
+part1 file = sum . map (uncurry (*)) . unwrap . parse (parseMemory parseMul) file <$> readFile file
 
 part2 :: FilePath -> IO Int
-part2 file = do
-  contents <- readFile file
-  return . snd . foldl condSum (True, 0) . map (right $ uncurry (*)) . rights . map (parse parseCondMul mempty) . getAllTextMatches $ contents =~ "mul\\([0-9]{1,3},[0-9]{1,3}\\)|do\\(\\)|don't\\(\\)"
+part2 file = snd . foldl condSum (True, 0) . map (right $ uncurry (*)) . unwrap . parse (parseMemory parseCondMul) file <$> readFile file
   where
     condSum (_, acc) (Left True) = (True, acc)
     condSum (_, acc) (Left False) = (False, acc)
     condSum (True, acc) (Right val) = (True, acc + val)
     condSum (False, acc) (Right _) = (False, acc)
+
+unwrap :: Either a [b] -> [b]
+unwrap (Left _) = []
+unwrap (Right x) = x
+
+parseMemory :: Parser a -> Parser [a]
+parseMemory parser = catMaybes <$> many (try (Just <$> parser) <|> (anyChar $> Nothing))
 
 parseMul :: Parser (Int, Int)
 parseMul = do
@@ -32,5 +34,4 @@ parseCond :: Parser Bool
 parseCond = try (string "do()" $> True) <|> try (string "don't()" $> False)
 
 parseCondMul :: Parser (Either Bool (Int, Int))
-parseCondMul = do
-  Left <$> parseCond <|> Right <$> parseMul
+parseCondMul = Left <$> parseCond <|> Right <$> parseMul

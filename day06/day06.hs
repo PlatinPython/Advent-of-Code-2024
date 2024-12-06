d8118c4f196061202aaa4e67853e3fc8516fc89c
PlatinPython <robert.alexander.kaempf@gmail.com>
2024-12-06 21:00:46 +0100

    Optimize Day 6

--- day06/day06.hs
+++ day06/day06.hs
@@ -1,23 +1,31 @@
 import Control.Monad.State (State, evalState, execState, get, put)
-import Data.Set (Set, empty, insert, member)
+import Data.Set (Set, empty, fromList, insert, member, toList)
+
+main :: IO ()
+main = do
+  putStrLn . ("Part 1: " ++) . show =<< part1 "day06/input.txt"
+  putStrLn . ("Part 2: " ++) . show =<< part2 "day06/input.txt"
 
 part1 :: FilePath -> IO Int
 part1 file = do
   map <- lines <$> readFile file
+  let size = (length map, length $ head map)
   let [pos] = [(y, x) | y <- [0 .. length map - 1], x <- [0 .. length (map !! y) - 1], (map !! y) !! x == '^']
-  let (map', _, _) = execState part1State (map, (pos, N), False)
-  return . sum $ length . filter (== 'X') <$> map'
+  let blocks = fromList [(y, x) | y <- [0 .. length map - 1], x <- [0 .. length (map !! y) - 1], (map !! y) !! x == '#']
+  let (set', _, _) = execState (part1State (blocks, size)) (empty, (pos, N), False)
+  return $ length set'
 
 part2 :: FilePath -> IO Int
 part2 file = do
   map <- lines <$> readFile file
+  let size = (length map, length $ head map)
   let [pos] = [(y, x) | y <- [0 .. length map - 1], x <- [0 .. length (map !! y) - 1], (map !! y) !! x == '^']
-  let (map', _, _) = execState part1State (map, (pos, N), False)
-  let options = [(y, x) | y <- [0 .. length map' - 1], x <- [0 .. length (map' !! y) - 1], (map' !! y) !! x == 'X', (y, x) /= pos]
-  let maps = [setPos '#' map pos | pos <- options]
-  return . length . filter id $ (evalState part2State . (,(pos, N),empty,False) <$> maps)
+  let blocks = fromList [(y, x) | y <- [0 .. length map - 1], x <- [0 .. length (map !! y) - 1], (map !! y) !! x == '#']
+  let (set', _, _) = execState (part1State (blocks, size)) (empty, (pos, N), False)
+  let maps = [insert pos blocks | pos <- toList set']
+  return . length . filter id $ (\x -> evalState (part2State (x, size)) ((pos, N), empty, False)) <$> maps
 
-type Map = [String]
+type Map = (Set Pos, (Int, Int))
 
 type Pos = (Int, Int)
 
@@ -25,53 +33,45 @@ data Dir = N | E | S | W deriving (Show, Eq, Ord)
 
 type DirPos = (Pos, Dir)
 
-part1State :: State (Map, DirPos, Bool) ()
-part1State = do
+part1State :: Map -> State (Set Pos, DirPos, Bool) ()
+part1State map = do
   let loop True = return ()
       loop False = do
-        (map, pos, done) <- get
-        let (map', pos', done') = move map pos
-        put (map', pos', done')
+        (set, pos, done) <- get
+        let (set', pos', done') = move map set pos
+        put (set', pos', done')
         loop done'
   loop False
 
-part2State :: State (Map, DirPos, Set DirPos, Bool) Bool
-part2State = do
+part2State :: Map -> State (DirPos, Set DirPos, Bool) Bool
+part2State map = do
   let loop True = return False
       loop False = do
-        (map, pos, set, done) <- get
-        let (map', pos', done') = move map pos
-        if not done' && member pos' set
+        (pos, seenSet, done) <- get
+        let (_, pos', done') = move map empty pos
+        if not done' && member pos' seenSet
           then
             return True
           else do
-            let set' = insert pos' set
-            put (map', pos', set', done')
+            let seenSet' = insert pos' seenSet
+            put (pos', seenSet', done')
             loop done'
   loop False
 
-move :: Map -> DirPos -> (Map, DirPos, Bool)
-move map ((y, x), N)
-  | y == 0 = (setPos 'X' map (y, x), ((y, x), N), True)
-  | (map !! (y - 1)) !! x == '#' = (map, ((y, x), E), False)
-  | otherwise = (setPos 'X' map (y, x), ((y - 1, x), N), False)
-move map ((y, x), E)
-  | x == length (map !! y) - 1 = (setPos 'X' map (y, x), ((y, x), E), True)
-  | (map !! y) !! (x + 1) == '#' = (map, ((y, x), S), False)
-  | otherwise = (setPos 'X' map (y, x), ((y, x + 1), E), False)
-move map ((y, x), S)
-  | y == length map - 1 = (setPos 'X' map (y, x), ((y, x), S), True)
-  | (map !! (y + 1)) !! x == '#' = (map, ((y, x), W), False)
-  | otherwise = (setPos 'X' map (y, x), ((y + 1, x), S), False)
-move map ((y, x), W)
-  | x == 0 = (setPos 'X' map (y, x), ((y, x), W), True)
-  | (map !! y) !! (x - 1) == '#' = (map, ((y, x), N), False)
-  | otherwise = (setPos 'X' map (y, x), ((y, x - 1), W), False)
-
-setPos :: Char -> Map -> Pos -> Map
-setPos c map (y, x) = changeList map y $ changeList (map !! y) x c
-  where
-    changeList :: [a] -> Int -> a -> [a]
-    changeList xs x val = before ++ (val : tail after)
-      where
-        (before, after) = splitAt x xs
+move :: Map -> Set Pos -> DirPos -> (Set Pos, DirPos, Bool)
+move (blocks, (ys, xs)) set ((y, x), N)
+  | y == 0 = (insert (y, x) set, ((y, x), N), True)
+  | member (y - 1, x) blocks = (set, ((y, x), E), False)
+  | otherwise = (insert (y, x) set, ((y - 1, x), N), False)
+move (blocks, (ys, xs)) set ((y, x), E)
+  | x == xs - 1 = (insert (y, x) set, ((y, x), E), True)
+  | member (y, x + 1) blocks = (set, ((y, x), S), False)
+  | otherwise = (insert (y, x) set, ((y, x + 1), E), False)
+move (blocks, (ys, xs)) set ((y, x), S)
+  | y == ys - 1 = (insert (y, x) set, ((y, x), S), True)
+  | member (y + 1, x) blocks = (set, ((y, x), W), False)
+  | otherwise = (insert (y, x) set, ((y + 1, x), S), False)
+move (blocks, (ys, xs)) set ((y, x), W)
+  | x == 0 = (insert (y, x) set, ((y, x), W), True)
+  | member (y, x - 1) blocks = (set, ((y, x), N), False)
+  | otherwise = (insert (y, x) set, ((y, x - 1), W), False)
